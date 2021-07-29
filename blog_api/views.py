ac8f184934add583916305309cebe3f3d2802da7
ephremworkeye <ephremworkeye@gmail.com>
2021-07-28 23:57:28 -0700

    post list and post detail view added, model registered and populated

--- blog_api/views.py
+++ blog_api/views.py
@@ -5,8 +5,9 @@ from .serializers import PostSerializer
 class PostList(generics.ListCreateAPIView):
     queryset = Post.postobjects.all()
     serializer_class = PostSerializer
-    pass
+    
 
 
-class PostDetail(generics.RetrieveDestroyAPIView):
-    pass
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.postobjects.all()
+    serializer_class = PostSerializer
